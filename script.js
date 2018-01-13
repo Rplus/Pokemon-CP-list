9eeb6f68bcdcc1e8b55d90c8a8afb8832a7a81c2
Rplus <rplus.tw@gmail.com>
2018-01-13 20:38:42 +0800

    enable service worker

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  // navigator.serviceWorker.register('service-worker.js');
+  navigator.serviceWorker.register('service-worker.js');
 }
 
 window.$ = document.querySelector.bind(document);
