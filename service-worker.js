75cc301d459d99c52790dce02398077f3e59a8c3
Rplus <rplus.tw@gmail.com>
2018-10-20 06:59:42 +0800

    update cached files

--- service-worker.js
+++ service-worker.js
@@ -14,13 +14,14 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2018-10-20.2';
+const PRECACHE = 'precache-v2018-10-20.3';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
+  './img/',
   'style.css',
   'levelMultiplier.json',
   'power-up.json',
