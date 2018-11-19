39b4870e55edfa9c4ac8bdaff11e04aff4b02fda
Rplus <rplus.tw@gmail.com>
2018-11-19 16:36:28 +0800

    update vue-cli config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,14 @@ module.exports = {
   baseUrl: process.env.NODE_ENV === 'production'
     ? '/Pokemon-CP-list/'
     : '/Pokemon-CP-list/',
-  outputDir: 'Pokemon-CP-list'
-}
+  outputDir: 'Pokemon-CP-list',
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config.module
+    .rule('images')
+      .test(/\.(png|jpe?g|gif|webp)(\?.*)?$/)
+      .use('url-loader')
+        .loader('url-loader')
+        .options({ limit: -1, name: '[path][name]_[hash:5].[ext]', })
+  },
+};
